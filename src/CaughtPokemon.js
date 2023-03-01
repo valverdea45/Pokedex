b4e937cf7df5a46f4c14617536b775bfa2fb7059
Adrian <adrianvalverde45@gmail.com>
2023-03-01 10:30:50 -0500

    sort button

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,16 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 import Pokemon from "./Pokemon"
 
 function CaughtPokemon({ caughtPokemon }) {
+    const [caughtPokemons, setCaughtPokemons] = useState([])
 
-    const pokemonCards = caughtPokemon.map((individualPokemon) => {
-        return <Pokemon key={individualPokemon.name} singlePokemon={individualPokemon}/>
+    setCaughtPokemons(caughtPokemon)
+
+
+    console.log(caughtPokemons)
+
+    function handleSortClick() {
+        setCaughtPokemons(caughtPokemons.map((pokemon) => {
+            return pokemon.name.sort()
+        }))
+    }
+
+    const pokemonCards = caughtPokemons.map((individualPokemon) => {
+        return <Pokemon key={individualPokemon.name} singlePokemon={individualPokemon} />
     })
 
     return (
         <div>
             <h1>Caught Pokemon</h1>
             <div>{`Total caught Pokemon ${caughtPokemon.length}.`}</div>
+            <button onClick={handleSortClick}> Sort By Name</button>
             <div>{pokemonCards}</div>
         </div>
     )
