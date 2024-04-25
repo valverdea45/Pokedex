879c7c8a9b5d61a27c9cbc93c27ff8d986002899
Adrian <adrianvalverde45@gmail.com>
2024-04-25 13:48:07 -0400

    updated wildpokemon component and css

--- src/WildPokemon.js
+++ src/WildPokemon.js
@@ -11,47 +11,47 @@ function WildPokemon({ uncaughtPokemon, handleUpdatePokemon }) {
     // add conditional for loading useeffect runs after the initial render.
 
     // function for creating random integers
-    function getRandomInteger(min, max) {
-        return Math.round(Math.random() * (max - min) + min)
-    }
+    // function getRandomInteger(min, max) {
+    //     return Math.round(Math.random() * (max - min) + min)
+    // }
     // created state to change pokemon index on uncaughtPokemon array
-    const [pokemonIndex, setPokemonIndex] = useState(0)
+    // const [pokemonIndex, setPokemonIndex] = useState(0)
 
     // created a state for each conditional
-    const [pokemonCaught, setPokemonCaught] = useState(false)
-    const [pokemonRan, setPokemonRan] = useState(false)
-    const [triedToRun, setTriedToRun] = useState(false)
+    // const [pokemonCaught, setPokemonCaught] = useState(false)
+    // const [pokemonRan, setPokemonRan] = useState(false)
+    // const [triedToRun, setTriedToRun] = useState(false)
     // const [wasClicked, setWasClicked] = useState(false)
-    const [showWildText, setShowWildText] = useState(false)
-    const [showBagText, setShowBagText] = useState(false)
+    const [showGrassGlowText, setShowGrassGlowText] = useState(false)
+    const [showBagGlowText, setShowBagGlowText] = useState(false)
     // created a state to display the game
     // const [couldYouRun, setCouldYouRun] = useState(false)
     //current random pokemon
-    let randomPokemon = uncaughtPokemon[pokemonIndex]
+    // let randomPokemon = uncaughtPokemon[pokemonIndex]
 
 
     // these useEffects are to reset states back to default
-    useEffect(() => {
-        setPokemonIndex(getRandomInteger(0, uncaughtPokemon.length - 1))
-    }, [uncaughtPokemon])
+    // useEffect(() => {
+    //     setPokemonIndex(getRandomInteger(0, uncaughtPokemon.length - 1))
+    // }, [uncaughtPokemon])
 
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            setPokemonCaught(false)
-        }, 1500)
-        return () => {
-            clearTimeout(timeoutId)
-        }
-    }, [pokemonCaught])
+    // useEffect(() => {
+    //     const timeoutId = setTimeout(() => {
+    //         setPokemonCaught(false)
+    //     }, 1500)
+    //     return () => {
+    //         clearTimeout(timeoutId)
+    //     }
+    // }, [pokemonCaught])
 
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            setPokemonRan(false)
-        }, 1500)
-        return () => {
-            clearTimeout(timeoutId)
-        }
-    }, [pokemonRan])
+    // useEffect(() => {
+    //     const timeoutId = setTimeout(() => {
+    //         setPokemonRan(false)
+    //     }, 1500)
+    //     return () => {
+    //         clearTimeout(timeoutId)
+    //     }
+    // }, [pokemonRan])
 
     // useEffect(() => {
     //     const timeoutId = setTimeout(() => {
@@ -62,14 +62,14 @@ function WildPokemon({ uncaughtPokemon, handleUpdatePokemon }) {
     //     }
     // }, [showHome])
 
-    useEffect(() => {
-        const timeoutId = setTimeout(() => {
-            setTriedToRun(false)
-        }, 1000);
-        return () => {
-            clearTimeout(timeoutId)
-        }
-    }, [triedToRun])
+    // useEffect(() => {
+    //     const timeoutId = setTimeout(() => {
+    //         setTriedToRun(false)
+    //     }, 1000);
+    //     return () => {
+    //         clearTimeout(timeoutId)
+    //     }
+    // }, [triedToRun])
 
     // useEffect(() => {
     //     const timeoutId = setTimeout(() => {
@@ -155,28 +155,32 @@ function WildPokemon({ uncaughtPokemon, handleUpdatePokemon }) {
     return (
         <body className="container">
                 <div>
-                    {showBagText ?
-                        <div>
-                            <img className="bag-container" onMouseLeave={() => setShowBagText(false)} onClick={() => navigate("/Bag")} src="https://t4.ftcdn.net/jpg/05/72/63/79/360_F_572637924_Q8biPcRDh9V3HNVL8ojaZwwc59t1Qx9p.jpg" alt="bag"/>
-                            <p>Restock on items!</p>
+                    {showBagGlowText ?
+                        <div className="hover minor-container" onMouseLeave={() => setShowBagGlowText(false)}>
+                            <img className="image" onClick={() => navigate("/Bag")} src="https://t4.ftcdn.net/jpg/05/72/63/79/360_F_572637924_Q8biPcRDh9V3HNVL8ojaZwwc59t1Qx9p.jpg" alt="bag"/>
+                            <p className="glow-text text">Restock on items!</p>
                         </div>
                         :
-                        <div>
-                            <img onMouseEnter={() => setShowBagText(true)} src="https://t4.ftcdn.net/jpg/05/72/63/79/360_F_572637924_Q8biPcRDh9V3HNVL8ojaZwwc59t1Qx9p.jpg" alt="bag" />
+                        <div className="hover minor-container"onMouseEnter={() => setShowBagGlowText(true)}>
+                            <img className="image" src="https://t4.ftcdn.net/jpg/05/72/63/79/360_F_572637924_Q8biPcRDh9V3HNVL8ojaZwwc59t1Qx9p.jpg" alt="bag" />
+                            <p className="text">Restock on items!</p> 
                         </div>
                     }
                 </div>
 
                 <div>
-                    {showWildText
-                        ?
-                        <div>
-                            <img  onMouseLeave={() => setShowWildText(false)} onClick={() => navigate("/WildEncounter")} src="https://static.vecteezy.com/system/resources/thumbnails/022/351/056/small_2x/grass-clipart-grass-transparent-background-free-png.png" alt="grass">
+                    {showGrassGlowText ?
+                        <div className="hover minor-container" onMouseLeave={() => setShowGrassGlowText(false)}>
+                            <img className="image" onClick={() => navigate("/WildEncounter")} src="https://static.vecteezy.com/system/resources/thumbnails/022/351/056/small_2x/grass-clipart-grass-transparent-background-free-png.png" alt="grass">
                             </img>
-                            <p>Find a wild pokemon!</p>
+                            <p className="glow-text text">Find a wild pokemon!</p>
                         </div>
                         :
-                        <img onMouseEnter={() => setShowWildText(true)} src="https://static.vecteezy.com/system/resources/thumbnails/022/351/056/small_2x/grass-clipart-grass-transparent-background-free-png.png" alt="grass" />
+                        <div className="hover minor-container" onMouseEnter={() => setShowGrassGlowText(true)}>
+                          <img className="image" src="https://static.vecteezy.com/system/resources/thumbnails/022/351/056/small_2x/grass-clipart-grass-transparent-background-free-png.png" alt="grass" />  
+                          <p className="text">Find a wild pokemon!</p>
+                        </div>
+                        
                     }
                 </div>
         </body>
