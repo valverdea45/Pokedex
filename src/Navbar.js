ad3bebf5fe4832e59cf3a4769305b228f58d281b
Adrian <adrianvalverde45@gmail.com>
2024-06-28 15:13:52 -0400

    updated the navbar with pokeballs and gradient

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,39 +3,31 @@ import { NavLink } from "react-router-dom";
 import './css/NavBar.css';
 
 
-// const linkStyles = {
-//     display: "inline-block",
-//     padding: "12px",
-//     margin: "0 6px 6px",
-//     background: "blue",
-//     textDecoration: "none",
-//     color: "white",
-// }
-
-const hoverStyle = {
-    background: "darkblue"
-}
 
 function Navbar() {
     return (
-        <div className="flex">
-            <img src="https://www.freeiconspng.com/thumbs/pokeball-png/pokeball-transparent-png-2.png" className="pokeball"/>
-            <NavLink to="/CaughtPokemon" exact className="linkStyles" activeStyle={hoverStyle}>
+        <div>
+            <div className="flex">
+                <img src="https://www.freeiconspng.com/thumbs/pokeball-png/pokeball-transparent-png-2.png" className="pokeball-left" alt="pokeballs" />
+                <NavLink to="/CaughtPokemon" exact="true" className="linkStyles" >
                 Caught Pokemon
-            </NavLink>
-            <NavLink to="/UncaughtPokemon" exact className="linkStyles" activeStyle={hoverStyle}>
+                </NavLink>
+                <NavLink to="/UncaughtPokemon" exact="true" className="linkStyles" >
                 Uncaught Pokemon
-            </NavLink>
-            <NavLink to="/WildPokemon" exact className="linkStyles" activeStyle={hoverStyle}>
-                Wild Pokemon
-            </NavLink>
-            <NavLink to="/Bag" exact className="linkStyles" activeStyle={hoverStyle}>
-                Bag
-            </NavLink>
-            <NavLink to="/AddPokemon" exact className="linkStyles" activeStyle={hoverStyle}>
-                Add Pokemon
-            </NavLink>
+                </NavLink>
+                <NavLink to="/WildPokemon" exact="true" className="linkStyles" >
+                    Wild Pokemon
+                </NavLink>
+                <NavLink to="/Bag" exact="true" className="linkStyles">
+                    Bag
+                </NavLink>
+                <NavLink to="/AddPokemon" exact="true" className="linkStyles">
+                    Add Pokemon
+                </NavLink>
+                <img src="https://www.freeiconspng.com/thumbs/pokeball-png/pokeball-transparent-png-2.png" className="pokeball-right" alt="pokeballs" />
+            </div>
         </div>
+
     )
 }
 
